19ccc1f9256bcca3d18a6b1e9164a9d0ac47f85d
sangshrestha <sang.socials@gmail.com>
2023-12-29 18:40:20 +0000

    feat: black formatting

--- msum.py
+++ msum.py
@@ -1,9 +1,11 @@
 import numpy as np
 
+
 def main():
-    A = np.array([4,-3,2,-1]).reshape(-1,2)
-    B = np.array([0,1,2,-2,3,1]).reshape(-1,3)
-    print(np.dot(A,B))
+    A = np.array([1, 2, 3, 1, 4, 1, 2, 1, 9]).reshape(-1, 3)
+    C = np.array([2, -1, 0, -1, 1, 0, -2, 1, 1]).reshape(-1, 3)
+    a, b = 4, -7
+    print(np.dot(C, A), sep="\n\n")
 
 
 main()
