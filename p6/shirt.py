51a5e4044b1355f95dc26e65e1c251f1385e77b7
sangshrestha <sang.socials@gmail.com>
2024-01-01 12:28:44 +0000

    feat: img compressions

--- p6/shirt.py
+++ p6/shirt.py
@@ -20,6 +20,40 @@ if in_suffix != out_suffix:
 
 shirt = Image.open("shirt.png")
 
+tiny_shirt = Image.open("tiny_shirt.png")
+tiny_data = list(tiny_shirt.getdata())
+new_shirt = Image.new("P", size=shirt.size)
+new_shirt.info = tiny_shirt.info
+new_shirt.putpalette(tiny_shirt.palette)
+data = list(shirt.getdata())
+
+
+def map_data(data):
+    new_data = []
+    count = 0
+    for d in data:
+        count += 1
+        if count > 300000:
+            print(d)
+            new_data.append((0, 0, 0, 0))
+        else:
+            new_data.append(d)
+
+    print(count)
+    return new_data
+
+
+def compare(data, tiny_data):
+    for i in range(len(data)):
+        if i > 300000:
+            print(data[i], tiny_data[i])
+
+
+# new_data = compare(data, tiny_data)
+
+new_shirt.putdata(tiny_data)
+new_shirt.save("new_shirt.png")
+
 try:
     with Image.open(sys.argv[1]) as input:
         input_crop = ImageOps.fit(input, shirt.size)
