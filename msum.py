e5406b7297d28a530ea6e4abf853ede25e907b4c
sangshrestha <sang.socials@gmail.com>
2024-08-15 23:23:08 +0100

    fix: delete unused folder

--- msum.py
+++ msum.py
@@ -2,11 +2,9 @@ import numpy as np
 
 
 def main():
-    A = np.array([1, 0, 0, -2, 3, 0, 2, 4, 1]).reshape(-1, 3)
-    B = np.array([2, -1, 3, 0, 1, 2, 0, 0, 4]).reshape(-1, 3)
+    A = np.array([0.5, -0.25, -0.25, 0.9]).reshape(-1, 2)
     C = np.array([7 / 4, 1, -1 / 2]).reshape(-1, 1)
-    a, b = 4, -7
-    print(np.dot(np.dot(A, B), C), sep="\n\n")
+    print(np.dot(np.linalg.inv(A), [7000, 14000]), sep="\n\n")
 
 
 if __name__ == "__main__":
